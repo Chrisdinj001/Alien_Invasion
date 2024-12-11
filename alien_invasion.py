437af64230f6b4b75a6696868acca6895b2bde35
Chrisdinj001 <chrisdinjones@gmail.com>
2024-12-11 02:41:48 -0500

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.sprite import Group
+
+
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    # Initialize game and create a screen object
+    pygame.init()
+    
+    pygame.display.set_caption("Alien Invasion")
+    
+    # Make the Play button.
+    
+    
+    # Create an instance to store game statistics and create a scoreboard.
+    
+    # Make a ship, a group of bullets, and a group of aliens
+  
+    bullets = Group()
+    aliens = Group()
+    
+    # Create the fleet of aliens.
+    
+    
+    # Start the main loop for the game.
+    
+        
+    
+        
+
+run_game()
